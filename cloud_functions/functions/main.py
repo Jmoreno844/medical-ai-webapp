37001a379e18fad984b10024e6cbd7d7b207e4a4
jmoreno844 <jmoreno844.dev@gmail.com>
2025-05-14 08:08:19 -0500

    new deploy

--- cloud_functions/functions/main.py
+++ cloud_functions/functions/main.py
@@ -5,7 +5,7 @@ Main entry point for Cloud Functions.
 import functions_framework
 from endpoints.transcription_endpoint import transcription_endpoint
 from endpoints.document_workflow import generate_document_workflow
-
+#
 # Export the Cloud Functions
 transcription_endpoint = functions_framework.http(transcription_endpoint)
 generate_document_workflow = functions_framework.http(generate_document_workflow)
