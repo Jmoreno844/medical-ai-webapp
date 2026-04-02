df3c99f27b1d19538a5d03a60399580329bcd5f0
jmoreno844 <jmoreno844.dev@gmail.com>
2026-04-01 21:39:26 -0500

    fix(infra): grant github deployer iam.serviceAccountUser

--- infra/modules/service_accounts/main.tf
+++ infra/modules/service_accounts/main.tf
@@ -133,6 +133,18 @@ resource "google_project_iam_member" "gh_cf_developer" {
   member  = "serviceAccount:${google_service_account.github_actions_deployer.email}"
 }
 
+resource "google_service_account_iam_member" "gh_cf_compute_sa_user" {
+  service_account_id = "projects/${var.project_id}/serviceAccounts/${data.google_project.current.number}-compute@developer.gserviceaccount.com"
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.github_actions_deployer.email}"
+}
+
+resource "google_project_iam_member" "gh_cf_sa_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.github_actions_deployer.email}"
+}
+
 resource "google_project_iam_member" "gh_ar_writer" {
   project = var.project_id
   role    = "roles/artifactregistry.writer"
